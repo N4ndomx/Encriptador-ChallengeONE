1231972ea758f410cbd78d5b1133e85b46d7f079
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-06-05 23:25:18 -0600

    v2 add encriptar y desencriptar

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 
 var textarea = document.getElementById('texto');
-
+var label = document.getElementById('resultado');
 function encriptar() {
-    let arr = textarea.value.split(" ");
-    let copy = arr.concat();
-    // console.log(copy)
-
-    arr.forEach((element, index) => {
-        let arry = Array.from(element);
-        for (let i = 0; i < arry.length; i++) {
-            if (arry[i] == 'a') {
-                arry[i] = 'ai';
-            }
-
-        }
-        copy[index] = arry.join("");
-    });
-
-    var resul = document.getElementById('resultado');
-    resul.innerText = copy.join(" ");
-    console.log(copy.join(" "));
-
+    let texto = textarea.value
+    var fraseModificada = texto.replace(/a/g, "ai").replace(/e/g, "enter")
+        .replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+    console.log(fraseModificada);
+    label.innerText = fraseModificada;
 }
 function desencriptar() {
-
+    let texto = textarea.value
+    var fraseModificada = texto.replace(/ai/g, "a").replace(/enter/g, "e")
+        .replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+    console.log(fraseModificada);
+    label.innerText = fraseModificada;
 }
 
 
